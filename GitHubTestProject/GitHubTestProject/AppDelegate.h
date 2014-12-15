85ad4d60646d139c7205d65dbe95be5605906974
wwn0989 <416891931@qq.com>
2014-12-15 16:29:33 +0800

    test

---
+++ GitHubTestProject/GitHubTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTestProject
+//
+//  Created by Peter.Wang on 15/12/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
